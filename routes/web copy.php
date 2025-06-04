64a5b92e853c096ef63ef0f91995f57e09947c60
sherlynclau <sherlynclaurensia_2327240134@mhs.mdp.ac.id>
2025-06-04 20:15:16 +0700

    brezee, npm install

--- routes/web copy.php
+++ routes/web copy.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\BarangKeluarController;
 use App\Http\Controllers\BarangMasukController;
@@ -10,13 +9,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::get('/profil', function () { //utk di web
+    return view ('profil'); //samain kek view
 });
-
-require __DIR__.'/auth.php';
 
 Route::resource('/barang', BarangController::class);
 Route::resource('/barangmasuk', BarangMasukController ::class);
